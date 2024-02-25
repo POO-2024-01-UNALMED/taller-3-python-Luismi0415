3c20f226e27b09eaf893303486929487f4c96ec2
Santiago Castro Herrera <159493032+Luismi0415@users.noreply.github.com>
2024-02-25 15:36:14 -0500

    1

---
+++ televisores/marca.py
@@ -0,0 +1,12 @@
+import televisores
+
+class Marca:
+    
+    def __init__(self,nombre):
+        self.nombre=nombre
+
+    def getNombre(self, nombre):
+        return nombre
+    
+    def setNombre(self, nombre):
+        self.nombre = nombre
